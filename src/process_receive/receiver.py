85b60bb5f7270fbd45750b32bc8ab54ea498e9a5
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2021-06-03 21:55:50 +0900

    Update receiver.py

--- src/process_receive/receiver.py
+++ src/process_receive/receiver.py
@@ -1,5 +1,22 @@
 import requests
+from datetime import date, timedelta
 
 
 class DataReceiver:
-    pass
+    def __init__(self):
+        self.yesterday = date.today() - timedelta(1)
+        self.host = 'https://www.koreaexim.go.kr/site/program/financial/exchangeJSON'
+        self.key = {'authkey': 'AJzeBrLvsmODLLS0JpVe9e0aFaKiTEt4',
+                    'searchdate': str(self.yesterday),
+                    'data': 'AP01'}
+
+    def api_process(self):
+        api = requests.get(self.host, self.key)
+        return api
+
+
+receiver = DataReceiver()
+response = receiver.api_process()
+print(response.status_code)
+print(response.json())
+print(response.text)
