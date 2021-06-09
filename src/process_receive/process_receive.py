fcd60e76e8aa7a78d38f6e9ca8cfc64d22a6b502
Ryu0n <hsung951027@gmail.com>
2021-06-09 17:10:46 +0900

    Save instances of response.

--- src/process_receive/process_receive.py
+++ src/process_receive/process_receive.py
@@ -19,9 +19,18 @@ class RatioReceiveProcess(RatioProcess):
     def run(self):
         receiver = DataReceiver()
         response = receiver.get(1)
-        print(response)
-        print(response.status_code)
-        print(response.json())
+        responses = response.json()
+
+        # 응답 객체 생성 후 리스트에 보관
+        for res in responses:
+            # https://dev-ryuon.tistory.com/4?category=908968 참고
+            v = res.values()
+            self.ratios.append(ExchangeRatio(*v))
+            print(v)
+
+        # 결과 확인
+        for r in self.ratios:
+            print(r.cur_unit)
 
 
 proc = RatioReceiveProcess()
