80d7ac039a1b384c075351cc2ed2b1bf4aeb094a
Shawn Hong <hsung951027@gmail.com>
2021-06-16 11:50:31 +0900

    Update visualizer.py

--- src/process_visualize/visualizer.py
+++ src/process_visualize/visualizer.py
@@ -1,26 +1,17 @@
 import matplotlib.pyplot as plt
-from src.process_receive.process_receive import proc
-from src.process_receive.receiver import DataReceiver
+from src.process_receive.process_receive import RatioReceiveProcess
+
+
 # 그래프 이미지형태로 뽑아내는법 차트, 히스토그램
 class DataVisualizer:
     def __init__(self):
-        data = DataReceiver()
-        self.day = data
-        self.day = data.get(0)
-        self.exchange = proc._ratios()
-
-    def func_x(self):
-        pass
-
-    def func_y(self):
-        pass
+        self.proc = RatioReceiveProcess()
+        self.proc.run()
 
     def run(self):
-        # func_x값에 임시로 숫자를 넣어줌
-        self.mat = plt.plot([self.day], self.exchange)
-        # return self.mat.show()
-        return self.mat
+        ratios = self.proc.ratios
+        print(ratios)
 
-
-d = DataVisualizer()
-d.run()
+        
+visualizer = DataVisualizer()
+visualizer.run()
