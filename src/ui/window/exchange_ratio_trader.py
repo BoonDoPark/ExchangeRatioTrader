4925d658c834597100027286918b0077e70df882
ryu0n <hsung951027@gmail.com>
2021-07-13 13:21:58 +0900

    create requirements.txt

--- src/ui/window/exchange_ratio_trader.py
+++ src/ui/window/exchange_ratio_trader.py
@@ -8,6 +8,6 @@
 # 2021-06-18 PyQT 설치 후 작업 진행 예정
 # FORM_CLASS, _ = uic.loadUiType()
 #
-# def WindowExchangeRatioTrader(QMainWindow, FORM_CLASS):
+# class WindowExchangeRatioTrader(QMainWindow, FORM_CLASS):
 #     pass
 
