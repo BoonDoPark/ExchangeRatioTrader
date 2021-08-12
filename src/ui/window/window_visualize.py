56a7f06bc6be5ed87ec787fadf424b7f8c19ea50
Ryu0n <hsung951027@gmail.com>
2021-08-12 15:00:54 +0900

    Edit error.

--- src/ui/window/window_visualize.py
+++ src/ui/window/window_visualize.py
@@ -89,36 +89,31 @@ class WindowVisualize(QMainWindow, FORM_CLASS):
                 selected_cur_unit_exchange_ratios.append(ExchangeRatio(kftc_deal_bas_r=0))
 
         # https://dev-ryuon.tistory.com/4?category=908968 : kwargs 참고
-        # selected_kftc_deal_bas_r = [float(exchange_ratio.kftc_deal_bas_r) for exchange_ratio in selected_cur_unit_exchange_ratios]
-        selected_kftc_deal_bas_r = []
-        for exchange_ratio in selected_cur_unit_exchange_ratios:
-            if exchange_ratio.kftc_deal_bas_r == 0:
-                selected_kftc_deal_bas_r.append(0)
-
-            elif ',' in exchange_ratio.kftc_deal_bas_r:
-                exchange_ratio = exchange_ratio.kftc_deal_bas_r.split(',')
-                exchange_ratio = ''.join(exchange_ratio)
-                selected_kftc_deal_bas_r.append(float(exchange_ratio))
-
-
-            else:
-                selected_kftc_deal_bas_r.append(float(exchange_ratio.kftc_deal_bas_r))
-
-        params = {'file_name': f'{key_dates[0]}-{key_dates[-1]}.png', 'xs': key_dates, 'ys': selected_kftc_deal_bas_r}
+        selected_kftc_deal_bas_r = [str(exchange_ratio.kftc_deal_bas_r) for exchange_ratio in selected_cur_unit_exchange_ratios]
+        params = {'file_name': f'{selected_cur_unit}.png', 'xs': key_dates, 'ys': selected_kftc_deal_bas_r}
         RatioVisualizeProcess.run(**params)
 
-        # names = []
-        # for exchange_ratio in exchange_ratios:
-        #     names.append({exchange_ratio.cur_unit: exchange_ratio.kftc_deal_bas_r})
+        self.pixmap = QPixmap()
+        self.pixmap.load(f'{selected_cur_unit}.png')
+        self.label_for_pixmap.setPixmap(self.pixmap)
+
+        # selected_kftc_deal_bas_r = []
+        # for exchange_ratio in selected_cur_unit_exchange_ratios:
+        #     if exchange_ratio.kftc_deal_bas_r == 0:
+        #         selected_kftc_deal_bas_r.append(0)
         #
-        #     self.save_file = RatioVisualizeProcess(f'{names[0][0]}.png', key_date, names[0][1])
-        #     self.save_file.run()
-        #     self.save_file._visualizer.export_to_img()
-        # image = QImage()
-        # image.load(f'{key_dates[0]}-{key_dates[-1]}.png')
-        pixmap = QPixmap()
-        pixmap.load(f'{key_dates[0]}-{key_dates[-1]}.png')
-        self.label_for_pixmap.setPixmap(pixmap)
-        # self.pixmap = QPixmap(f'{selected_cur_unit}.png')
-        # self.label = QLabel(self)
-        # self.label.setPixmap(self.pixmap)
+        #     elif ',' in exchange_ratio.kftc_deal_bas_r:
+        #         exchange_ratio = exchange_ratio.kftc_deal_bas_r.split(',')
+        #         exchange_ratio = ''.join(exchange_ratio)
+        #         selected_kftc_deal_bas_r.append(float(exchange_ratio))
+        #
+        #
+        #     else:
+        #         selected_kftc_deal_bas_r.append(float(exchange_ratio.kftc_deal_bas_r))
+        #
+        # params = {'file_name': f'{key_dates[0]}-{key_dates[-1]}.png', 'xs': key_dates, 'ys': selected_kftc_deal_bas_r}
+        # RatioVisualizeProcess.run(**params)
+        #
+        # pixmap = QPixmap()
+        # pixmap.load(f'{key_dates[0]}-{key_dates[-1]}.png')
+        # self.label_for_pixmap.setPixmap(pixmap)
