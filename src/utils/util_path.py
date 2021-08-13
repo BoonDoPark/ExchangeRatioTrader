af5b6b00219f2b28adf5b80278e91dd0eb9b09aa
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2021-08-13 16:46:42 +0900

    PathUtils에서 imgs폴더 저장 경로와 png파일을 imgs파일로 이동시키는 코드

--- src/utils/util_path.py
+++ src/utils/util_path.py
@@ -1,10 +1,27 @@
 import os
+import shutil
 
 
 class PathUtils:
     PROJECT_PATH = os.getcwd()
+    FOLDER_PATH = os.makedirs('../../../imgs', exist_ok=True)
 
     @classmethod
     def ui_path(cls, path: str):
         path = cls.PROJECT_PATH + f'\\{path}'
         return path
+
+    @classmethod
+    def folder_make_path(cls):
+        path = cls.FOLDER_PATH
+        return path
+
+    @classmethod
+    def img_save_path(cls, name: str, path: str):
+        cls.img_save = shutil.move(f'{name}', cls.PROJECT_PATH + f'\\{path}')
+        path = cls.img_save
+        return path
+
+    @classmethod
+    def img_read_path(cls, path: str):
+        pass
