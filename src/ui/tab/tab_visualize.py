de9153d2630bc23c7a252cb452372bbdae85bf52
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2021-09-01 17:53:01 +0900

    완료하였으나, window_exchangge_ratio_trader 실행은 tab_receive의 조회버튼, tab_visualze 리스트부분을 클릭시 꺼짐.의 오류 수정

--- src/ui/tab/tab_visualize.py
+++ src/ui/tab/tab_visualize.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidget, QTableWidgetItem, QSpinBox
 
 from src.common.data import ExchangeRatio
 from src.utils.ui_utils_table import QTableFormat, QTableWidgetUtils
@@ -29,7 +29,7 @@ class TabVisualize:
         self.window.tableWidget_country_list.setRowCount(0)
 
         # duration 전의 날짜에 대한 환율 정보들을 수집
-        duration = self.window.spinBox_duration.value()
+        duration = self.window.spinBox_duration_2.value()
         self.proc_receive.run(duration)
         key_date = DateUtils.before_duration(duration)
         exchange_ratios = self.proc_receive.ratios.get(key_date)
@@ -50,9 +50,9 @@ class TabVisualize:
         QTableWidgetUtils.resize_table_widget(self.window.tableWidget_country_list)
 
     def on_clicked_refresh_chart(self, item: QTableWidgetItem):
-        duration = self.window.spinBox_duration.value()
+        duration = self.window.spinBox_duration_2.value()
         key_dates = []
-        selected_exchange_ratio: ExchangeRatio = self.window.item.data(Qt.UserRole)
+        selected_exchange_ratio: ExchangeRatio = item.data(Qt.UserRole)
         selected_cur_unit = selected_exchange_ratio.cur_unit
 
         for d in range(duration, 0, -1):
@@ -67,21 +67,23 @@ class TabVisualize:
         for key_date in key_dates:
             # 각각의 일자로부터 선택된 아이템의 국가 코드와 일치하는 환율 정보들만 추출
             exchange_ratios = self.proc_receive.ratios.get(key_date)
-            filtered_exchange_ratios = list(filter(lambda exchange_ratio: exchange_ratio.cur_unit == selected_cur_unit, exchange_ratios))
+            filtered_exchange_ratios = list(
+                filter(lambda exchange_ratio: exchange_ratio.cur_unit == selected_cur_unit, exchange_ratios))
             selected_cur_unit_exchange_ratios.extend(filtered_exchange_ratios)
             if not len(filtered_exchange_ratios):
                 # 주말에 대한 예외처리
                 selected_cur_unit_exchange_ratios.append(ExchangeRatio(kftc_deal_bas_r=0))
 
-            PathUtils.create_folder('imgs')
-            # https://dev-ryuon.tistory.com/4?category=908968 : kwargs 참고
-            selected_kftc_deal_bas_r = [str(exchange_ratio.kftc_deal_bas_r) for exchange_ratio in
-                                        selected_cur_unit_exchange_ratios]
-            params = {'file_name': f'{PathUtils.img_path(selected_cur_unit)}.png', 'xs': key_dates,
-                      'ys': selected_kftc_deal_bas_r, 'x_label': f'{selected_cur_unit}', 'y_label': 'WON'}
-            RatioVisualizeProcess.run(**params)
+        PathUtils.create_folder('imgs')
+        # https://dev-ryuon.tistory.com/4?category=908968 : kwargs 참고
+        selected_kftc_deal_bas_r = [str(exchange_ratio.kftc_deal_bas_r) for exchange_ratio in
+                                    selected_cur_unit_exchange_ratios]
+        params = {'file_name': f'{PathUtils.img_path(selected_cur_unit)}.png', 'xs': key_dates,
+                  'ys': selected_kftc_deal_bas_r, 'x_label': f'{selected_cur_unit}', 'y_label': 'WON'}
+        RatioVisualizeProcess.run(**params)
 
-            self.pixmap = QPixmap()
-            self.pixmap.load(f'{PathUtils.img_path(selected_cur_unit)}.png')
-            # self.pixmap.load(f'{selected_cur_unit}.png')
-            self.window.label_for_pixmap.setPixmap(self.pixmap)
+        self.pixmap = QPixmap()
+        self.pixmap.load(f'{PathUtils.img_path(selected_cur_unit)}.png')
+        # self.pixmap.load(f'{selected_cur_unit}.png')
+        self.window.label_for_pixmap.setPixmap(self.pixmap)
+
